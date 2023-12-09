d0b4baeedd0587c0e66f5a60d7941732ea2e73f7
herculesnatan <medeiroshercules23@gmail.com>
2023-12-08 21:43:51 -0300

    CardComida

---
+++ cocinaborcelle/src/components/cardComida.tsx
@@ -0,0 +1,14 @@
+import styles from "@/styles/CardComida.module.css"
+
+export default function CardComida() {
+    return (
+    <body>
+        <div className={styles.CardComida}>
+            <img src="macarrao.jpg"></img>
+            <h1>Macarrão ao Sugo</h1>
+            <h2>Descrição</h2>
+            <button></button>
+        </div>
+</body>
+    );
+}

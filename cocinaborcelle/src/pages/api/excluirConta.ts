b8c8ff003e0b9513ad45482f72e8613ecde7363e
PedroAmorimG <pedroamorim696@gmail.com>
2023-12-09 10:02:50 -0300

    API de deletar usuario

---
+++ cocinaborcelle/src/pages/api/excluirConta.ts
@@ -0,0 +1,34 @@
+import { auth } from "@/../auth/lucia";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+	if (req.method != "POST") {
+		res.status(405).json({ error: "Bad Request" });
+		res.end();
+		return;
+	}
+
+	const authRequest = auth.handleRequest({ req, res });
+	const session = await authRequest.validate();
+
+	if (!session) {
+		res.status(401).json({ error: "Unauthorized" });
+		res.end();
+		return;
+	}
+
+	try {
+		const userId = session.user.userId;
+		await auth.deleteUser(userId);
+	} catch (e) {
+		res.status(500).json({ error: e });
+		res.end();
+		return;
+	}
+
+	res.status(200).json("Conta excluida com sucesso");
+	res.end();
+	return;
+};
+
+export default handler;

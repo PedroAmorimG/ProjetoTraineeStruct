c408b9574ca0e44947c5bec93bca255bca70d1de
herculesnatan <medeiroshercules23@gmail.com>
2023-12-10 10:27:59 -0300

    CardComida

--- cocinaborcelle/src/components/cardComida.tsx
+++ cocinaborcelle/src/components/cardComida.tsx
@@ -1,14 +1,34 @@
-import styles from "@/styles/CardComida.module.css"
+import { useState } from 'react';
+import styles from "@/styles/CardComida.module.css";
 
-export default function CardComida() {
+type CardComidaProps = {
+    title?: string;
+}
+
+export default function CardComida({ title }: CardComidaProps) {
+    const [count, setCount] = useState(0);
+
+    const handleIncrement = () => {
+        setCount(count + 1);
+    }
+
+    const handleDecrement = () => {
+        if (count > 0) {
+            setCount(count - 1);
+        }
+    }
+
     return (
-    <body>
         <div className={styles.CardComida}>
-            <img src="macarrao.jpg"></img>
-            <h1>Macarrão ao Sugo</h1>
+            <img src="macarrao.jpg" alt={title} />
+            <h1>{title}</h1>
             <h2>Descrição</h2>
-            <button></button>
+            <h2>Preço R$ 00,00 <button onClick={handleDecrement}>-</button>
+            <span>{count}</span>
+            <button onClick={handleIncrement}>+</button></h2>
+            
+            
         </div>
-</body>
+
     );
 }
